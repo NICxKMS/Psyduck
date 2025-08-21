d0f9b62dc27b4390e84f86e815e571de5f0068a3
Nikhil Kumar <121129363+NICxKMS@users.noreply.github.com>
2025-08-21 12:02:46 +0530

    Update deploy script in package.json to remove unnecessary 'dist' directory reference

---
+++ vite.config.ts
@@ -0,0 +1,68 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          radix: [
+            '@radix-ui/react-accordion',
+            '@radix-ui/react-alert-dialog',
+            '@radix-ui/react-aspect-ratio',
+            '@radix-ui/react-avatar',
+            '@radix-ui/react-checkbox',
+            '@radix-ui/react-collapsible',
+            '@radix-ui/react-context-menu',
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-dropdown-menu',
+            '@radix-ui/react-hover-card',
+            '@radix-ui/react-label',
+            '@radix-ui/react-menubar',
+            '@radix-ui/react-navigation-menu',
+            '@radix-ui/react-popover',
+            '@radix-ui/react-progress',
+            '@radix-ui/react-radio-group',
+            '@radix-ui/react-scroll-area',
+            '@radix-ui/react-select',
+            '@radix-ui/react-separator',
+            '@radix-ui/react-slider',
+            '@radix-ui/react-switch',
+            '@radix-ui/react-tabs',
+            '@radix-ui/react-toast',
+            '@radix-ui/react-toggle',
+            '@radix-ui/react-toggle-group',
+            '@radix-ui/react-tooltip',
+          ],
+          tanstack: [
+            '@tanstack/react-query',
+            '@tanstack/react-query-devtools',
+          ],
+          codemirror: [
+            '@codemirror/autocomplete',
+            '@codemirror/commands',
+            '@codemirror/lang-cpp',
+            '@codemirror/lang-css',
+            '@codemirror/lang-go',
+            '@codemirror/lang-html',
+            '@codemirror/lang-java',
+            '@codemirror/lang-javascript',
+            '@codemirror/lang-python',
+            '@codemirror/lang-rust',
+            '@codemirror/lang-sql',
+            '@codemirror/language',
+            '@codemirror/lint',
+            '@codemirror/search',
+            '@codemirror/state',
+            '@codemirror/view',
+          ],
+        },
+      },
+    },
+  },
+});
+
+
